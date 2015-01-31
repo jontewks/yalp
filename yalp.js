99b66b5ff31eb41c17c952a78589c6b9fd38a2b0
Jon Tewksbury <jontewks@gmail.com>
2015-01-30 20:11:26 -0800

    Grabbing data we want correctly.

--- yalp.js
+++ yalp.js
@@ -10,6 +10,22 @@ http.get('http://www.yelp.com/biz/bliss-coffee-redwood-city', function(res) {
 
   res.on('end', function() {
     var $ = cheerio.load(str);
-    console.log($('div').length);
+    $('.review-wrapper .review-content').each(function(i, el) {
+      var rating, date, description;
+
+      $(el).find($('meta[itemprop="ratingValue"]')).each(function(i, el) {
+        rating = el.attribs.content;
+      });
+
+      $(el).find($('meta[itemprop="datePublished"]')).each(function(i, el) {
+        date = el.attribs.content;
+      });
+
+      $(el).find($('p[itemprop="description"]')).each(function(i, el) {
+        description = $(el).text();
+      });
+
+      console.log(rating, date, description)
+    });
   });
 })
